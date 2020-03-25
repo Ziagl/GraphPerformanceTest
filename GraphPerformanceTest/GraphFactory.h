78cc2b78694d4a6914855ff678b8a906be348335
Ziagl <office@ziegelwanger-edv.at>
2020-03-25 17:12:19 +0100

    completed all task for graph data structure

--- GraphPerformanceTest/GraphFactory.h
+++ GraphPerformanceTest/GraphFactory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <vector>
 #include <map>
 #include <list>
@@ -11,33 +12,36 @@
 #include "Team.h"
 #include "Player.h"
 
-struct GraphData
+struct GraphDataFacility
 {
-    virtual std::string toString() { return "root"; };
+    virtual ~GraphDataFacility() = default;
+    virtual std::string toString() = 0;
 };
 
-struct GraphCountry : public Country, public GraphData
+using GraphData = std::shared_ptr<GraphDataFacility>;
+
+struct GraphCountry : public Country, public GraphDataFacility
 {
     GraphCountry(std::string name) : Country(name) {}
-    virtual std::string toString() { return Name(); }
+    virtual std::string toString() override { return Name(); }
 };
 
-struct GraphLeague : public League, public GraphData
+struct GraphLeague : public League, public GraphDataFacility
 {
     GraphLeague(std::string name) : League(name) {}
-    virtual std::string toString() { return Name(); }
+    virtual std::string toString() override { return Name(); }
 };
 
-struct GraphTeam : public Team, public GraphData
+struct GraphTeam : public Team, public GraphDataFacility
 {
     GraphTeam(std::string name) : Team(name) {}
-    virtual std::string toString() { return Name(); }
+    virtual std::string toString() override { return Name(); }
 };
 
-struct GraphPlayer : public Player, public GraphData
+struct GraphPlayer : public Player, public GraphDataFacility
 {
     GraphPlayer(std::string firstname, std::string lastname) : Player(firstname, lastname) {}
-    virtual std::string toString() { return Firstname() + " " + Lastname(); }
+    virtual std::string toString() override { return Firstname() + " " + Lastname(); }
 };
 
 enum class Node_type { COUNTRY, LEAGUE, TEAM, PLAYER, UNKNOWN };
@@ -59,13 +63,16 @@ public:
     VertexProperty(int id = -1, Node_type type = Node_type::UNKNOWN) : id(id), type(type) { }
     VertexProperty(int id, Node_type type, GraphData data) : id(id), type(type), data(data) { }
 
-    std::string toString() const {
+    std::string toString() const 
+    {
         std::ostringstream oss;
         oss << *this;
         return oss.str();
     }
 
     int getVertexID() { return id; }
+    Node_type getType() { return type; }
+    GraphData getData() { return data; }
 
 private:
     friend std::ostream& operator<<(std::ostream& os, VertexProperty const& v) {
@@ -77,13 +84,25 @@ private:
     GraphData data;
 };
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexProperty> Graph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, VertexProperty> Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
 typedef boost::graph_traits<Graph>::vertex_iterator vertex_iterator;
+typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
+typedef boost::graph_traits<Graph>::in_edge_iterator in_edge_iterator;
+typedef boost::graph_traits<Graph>::out_edge_iterator out_edge_iterator;
 
 class GraphFactory
 {
 public:
 	static Graph create();
-    static void showVertices(Graph g);
+    static void showVertices(const Graph &g, bool listVertices = false);
+    static void showEdges(const Graph &g, bool listEdges = false);
+    static void findPlayer(Graph& g, const std::tuple<std::string, std::string> name);
+    static void listRandomTeam(Graph& g);
+    static void moveRandomPlayerToRandomTeam(Graph& g);
+
+private:
+    static std::vector<vertex_t> getParentIds(Graph& g, vertex_t vertex);
+    static std::vector<vertex_t> getChildIds(Graph& g, vertex_t vertex);
+    static std::vector<vertex_t> findVerticesOfType(Graph& g, Node_type type);
 };
--- GraphPerformanceTest/main.cpp
+++ GraphPerformanceTest/main.cpp
@@ -26,66 +26,84 @@ int main()
     NameGenerator nameGenerator;
 
     // create class structure data
-    auto data = ClassHierarchyFactory::create();
+    {
+        std::cout << "create class data structure" << std::endl;
+        std::cout << "***************************" << std::endl << std::endl;
+        auto data = ClassHierarchyFactory::create();
 
-    // search for a random name
-    auto randomName = nameGenerator.createName();
-    std::cout << "Search started for: " << std::get<1>(randomName) << std::endl;
-    std::for_each(data.begin(), data.end(), [randomName](auto country) {
-        //std::cout << country.Name() << std::endl;
-        std::for_each(country.leagues.begin(), country.leagues.end(), [randomName, country](auto league) mutable {
-            //std::cout << league.Name() << std::endl;
-            std::for_each(league.teams.begin(), league.teams.end(), [randomName, country, league](auto team) mutable {
-                //std::cout << team.Name() << std::endl;
-                std::for_each(team.player.begin(), team.player.end(), [randomName, country, league, team] (auto player) mutable {
-                    if (std::get<1>(randomName) == player.Lastname())
-                    {
-                        std::cout << "    " << player.Firstname() << " " << player.Lastname() << " (" << team.Name() << ", " << league.Name() << ", " << country.Name() << ")" << std::endl;
-                    }
+        // search for a random name
+        auto randomName = nameGenerator.createName();
+        std::cout << "Search started for: " << std::get<1>(randomName) << std::endl;
+        std::for_each(data.begin(), data.end(), [randomName](auto country) {
+            //std::cout << country.Name() << std::endl;
+            std::for_each(country.leagues.begin(), country.leagues.end(), [randomName, country](auto league) mutable {
+                //std::cout << league.Name() << std::endl;
+                std::for_each(league.teams.begin(), league.teams.end(), [randomName, country, league](auto team) mutable {
+                    //std::cout << team.Name() << std::endl;
+                    std::for_each(team.player.begin(), team.player.end(), [randomName, country, league, team](auto player) mutable {
+                        if (std::get<1>(randomName) == player.Lastname())
+                        {
+                            std::cout << "    " << player.Firstname() << " " << player.Lastname() << " (" << team.Name() << ", " << league.Name() << ", " << country.Name() << ")" << std::endl;
+                        }
+                        });
+                    });
                 });
             });
-        });
-    });
 
-    // list players of a random team
-    int randomCountry = RandomNumberGenerator::randomNumber(0, static_cast<int>(data.size()));
-    auto country = data[randomCountry];
-    int randomLeague = RandomNumberGenerator::randomNumber(0, static_cast<int>(country.leagues.size()));
-    auto league = country.leagues[randomLeague];
-    int randomTeam = RandomNumberGenerator::randomNumber(0, static_cast<int>(league.teams.size()));
-    auto team = league.teams[randomTeam];
-    std::cout << "List " << team.Name() << " (" << league.Name() << ", " << country.Name() << ")" << std::endl;
-    std::for_each(team.player.begin(), team.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
+        // list players of a random team
+        int randomCountry = RandomNumberGenerator::randomNumber(0, static_cast<int>(data.size()));
+        auto country = data[randomCountry];
+        int randomLeague = RandomNumberGenerator::randomNumber(0, static_cast<int>(country.leagues.size()));
+        auto league = country.leagues[randomLeague];
+        int randomTeam = RandomNumberGenerator::randomNumber(0, static_cast<int>(league.teams.size()));
+        auto team = league.teams[randomTeam];
+        std::cout << "List " << team.Name() << " (" << league.Name() << ", " << country.Name() << ")" << std::endl;
+        std::for_each(team.player.begin(), team.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
 
-    // move random player to random team
-    int randomPlayer = RandomNumberGenerator::randomNumber(0, static_cast<int>(team.player.size()));
-    auto player = team.player[randomPlayer];
-    std::cout << "Move Player " << player.Firstname() << " " << player.Lastname() << " from " << team.Name() << " (" << league.Name() << ", " << country.Name() << ") to ";
-    int randomCountryNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(data.size()));
-    auto countryNew = data[randomCountryNew];
-    int randomLeagueNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(country.leagues.size()));
-    auto leagueNew = country.leagues[randomLeagueNew];
-    int randomTeamNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(league.teams.size()));
-    auto teamNew = league.teams[randomTeamNew];
-    std::cout << teamNew.Name() << " (" << leagueNew.Name() << ", " << countryNew.Name() << ")" << std::endl;
+        // move random player to random team
+        int randomPlayer = RandomNumberGenerator::randomNumber(0, static_cast<int>(team.player.size()));
+        auto player = team.player[randomPlayer];
+        std::cout << "Move Player " << player.Firstname() << " " << player.Lastname() << " from " << team.Name() << " (" << league.Name() << ", " << country.Name() << ") to ";
+        int randomCountryNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(data.size()));
+        auto countryNew = data[randomCountryNew];
+        int randomLeagueNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(country.leagues.size()));
+        auto leagueNew = country.leagues[randomLeagueNew];
+        int randomTeamNew = RandomNumberGenerator::randomNumber(0, static_cast<int>(league.teams.size()));
+        auto teamNew = league.teams[randomTeamNew];
+        std::cout << teamNew.Name() << " (" << leagueNew.Name() << ", " << countryNew.Name() << ")" << std::endl;
 
-    teamNew.player.insert(  teamNew.player.end(), 
-                            std::make_move_iterator(team.player.begin() + randomPlayer), 
-                            std::make_move_iterator(team.player.begin() + randomPlayer + 1)
-    );
-    team.player.erase(team.player.begin() + randomPlayer, team.player.begin() + randomPlayer + 1);
+        teamNew.player.insert(teamNew.player.end(),
+            std::make_move_iterator(team.player.begin() + randomPlayer),
+            std::make_move_iterator(team.player.begin() + randomPlayer + 1)
+            );
+        team.player.erase(team.player.begin() + randomPlayer, team.player.begin() + randomPlayer + 1);
 
-    std::cout << "List old " << team.Name() << " (" << league.Name() << ", " << country.Name() << ")" << std::endl;
-    std::for_each(team.player.begin(), team.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
-    std::cout << "List new " << teamNew.Name() << " (" << leagueNew.Name() << ", " << countryNew.Name() << ")" << std::endl;
-    std::for_each(teamNew.player.begin(), teamNew.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
+        std::cout << "List old " << team.Name() << " (" << league.Name() << ", " << country.Name() << ")" << std::endl;
+        std::for_each(team.player.begin(), team.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
+        std::cout << "List new " << teamNew.Name() << " (" << leagueNew.Name() << ", " << countryNew.Name() << ")" << std::endl;
+        std::for_each(teamNew.player.begin(), teamNew.player.end(), [](auto player) { std::cout << "    " << player.Firstname() << " " << player.Lastname() << std::endl; });
+    }
 
     // create graph structure data
-    auto graph = GraphFactory::create();
-    auto vertices = graph.m_vertices;
-    std::cout << "Graph mit " << vertices.size() << " Knoten erstellt." << std::endl;
+    {
+        std::cout << std::endl << std::endl;
+        std::cout << "create graph data structure" << std::endl;
+        std::cout << "***************************" << std::endl << std::endl;
+        auto graph = GraphFactory::create();
+        //GraphFactory::showVertices(graph);
+        //GraphFactory::showEdges(graph);
 
-    GraphFactory::showVertices(graph);
+        // search for a random name
+        auto randomName = nameGenerator.createName();
+        std::cout << "Search started for: " << std::get<1>(randomName) << std::endl;
+        GraphFactory::findPlayer(graph, randomName);
+
+        // list players of a random team
+        GraphFactory::listRandomTeam(graph);
+
+        // move random player to random team
+        GraphFactory::moveRandomPlayerToRandomTeam(graph);
+    }
 
     return 0;
 }
